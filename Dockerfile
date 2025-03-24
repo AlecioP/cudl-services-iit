ef05304b6a5e521cea74ef4f440f74b9a3939825
Alessio Portaro <alecioportaro@gmail.com>
2025-03-24 15:23:10 +0100

    Schemas and stylesheets on iit instance of gitlab. Just data in local server. Schema to use for validation extracted from xsi:noNamespaceSchemaLocation and stylesheet processing instruction href altered to be same origin compliant (route in secure proxy to mirror gitlab)

--- Dockerfile
+++ Dockerfile
@@ -10,15 +10,10 @@ COPY requirements.txt ./
 
 RUN pip install --no-cache-dir -r requirements.txt
 
-RUN echo -n 'SECRET_KEY="' > config_key.py
-RUN python -c 'import secrets; print(secrets.token_hex(),end="")' >> config_key.py
-RUN echo '"' >>config_key.py
-COPY config.py ./
-# Append newline
-RUN echo " " >> config.py 
-RUN cat config_key.py >> config.py
+RUN echo -n 'SECRET_KEY="' > config.py
+RUN python -c 'import secrets; print(secrets.token_hex(),end="")' >> config.py
+RUN echo '"' >>config.py
 
-COPY apiresource.py ./
 
 ENTRYPOINT waitress-serve --threads $WAITRESS_THREADS --port 5000 wsgi:app >> ./flask.out 2>&1
 #ENTRYPOINT python -m flask --app wsgi.py run --host=0.0.0.0 --debug >> ./flask.out 2>&1
--- wsgi.py
+++ wsgi.py
@@ -1,25 +1,33 @@
-from apiresource import apiXmlResourceConverter, apiXmlResource
-
+from os import abort
 from pathlib import Path
+import xml.dom.minidom
+import urllib.parse
 
+import flask
+from lxml import etree
 from flask import Flask, Response #,send_file
 
 app = Flask(__name__,template_folder="data/static")
 
 from flask import send_from_directory, render_template
 
-from lxml import etree
-from saxonche import PySaxonProcessor #, PySaxonApiError
-    
-app.url_map.converters.update(api_xmlResource=apiXmlResourceConverter)
+
+import os
+import requests
+
+
+# https://www.mail-archive.com/lxml@python.org/msg00035.html
+class SimpleResolver(etree.Resolver):
+    def resolve(self, url, pubid, context):
+        res = requests.get(url)
+        return self.resolve_string(res.text, context)
 
 def handle_bad_request(e):
-    #notfound_page_path = "./data/static/404.html" # changed in flask app constructor TODO should load path from config JSON
+    # TODO should load path from config JSON
+    # notfound_page_path = "./data/static/404.html" # changed in flask app constructor
     return render_template("404.html"), 404
 
 app.register_error_handler(404, handle_bad_request)
-
-from config import MAP_RUOTE_TO_FILESYSTEM
 
 @app.route('/v1/transcriptions/plain/<path:path>')
 def send_transcription_plain(path):
@@ -27,118 +35,62 @@ def send_transcription_plain(path):
 
 @app.route('/v1/transcriptions/structured/<docType>/<yearVersion>/<docKey>')
 def send_transcription_struct_saxon(docType,yearVersion,docKey):
-    # FIXME directory traversal attack
-    STRUCT_TRASCR_PREFIX = "./data/transcr/struct/"
-
-    data_xml_path = STRUCT_TRASCR_PREFIX + docKey + "/data.xml"
-
-    schema_path = MAP_RUOTE_TO_FILESYSTEM["prefisso-filesystem"] + MAP_RUOTE_TO_FILESYSTEM["tipologie-documentali"][docType]["versioni-anni"][yearVersion]["tipologie-risorse"][apiXmlResource.SCHEMA.value]
-
-    transform_path = MAP_RUOTE_TO_FILESYSTEM["prefisso-filesystem"] + MAP_RUOTE_TO_FILESYSTEM["tipologie-documentali"][docType]["versioni-anni"][yearVersion]["tipologie-risorse"][apiXmlResource.STYLE.value]
-
-    with PySaxonProcessor(license=False) as proc:
-        xsltproc = proc.new_xslt30_processor()
-        with open(data_xml_path,"r") as f:
-            document = proc.parse_xml(xml_text=f.read())
-
-        # IF data.xml is a DOCTYPE1/YEAR1 should be validated with the 
-        # proper schema and processed with the proper XSL
-        # 
-        # Anyway nothing prevents one for making a request to the url
-        # DOCTYPE2/YEAR2/DOCID_1
-        # this way DOCID_1 which is valid against schema DOCTYPE1/YEAR1
-        # would be processed with XSL for DOCTYPE2/YEAR2
-        # which could lead to unwanted results.
-        #
-        # To solve this we test that the xml data validates against the
-        # schema for the type DOCTYPE2/YEAR2 which is not true
-
-
-        # ALL THIS IS NOT LICENSED WITH SAXONC home edition 
-        # xsd_proc = proc.new_schema_validator()
-        # xsd_proc.register_schema(xsd_file=schema_path)
-        # try:
-        #     xsd_proc.validate(xdm_node=document)#(file_name=data_xml_path)
-        # except PySaxonApiError :
-        #     return "The document corresponding to the requested ID cannot be processed with the tools relative to the requested DOC type"
-        #
-        # REPLACING WITH lxml 
-        with open(data_xml_path) as f:
-            struct_data = etree.parse(f)
-        with open(schema_path) as f:
-            schema = etree.XMLSchema(etree.parse(f))
-        if not schema.validate(struct_data):
-            return "Semantic error in URL : The document corresponding to the requested ID cannot be processed with the tools relative to the requested DOC type"
-        
-        executable = xsltproc.compile_stylesheet(stylesheet_file=transform_path)
-        output = executable.transform_to_string(xdm_node=document)
-    return Response(output,mimetype="text/html")
-
-#@app.route('/v1/transcriptions/structured/<docType>/<yearVersion>/<docKey>')
-def send_transcription_struct(docType,yearVersion,docKey):
-
-    # TODO 
-    # as pointed out in this comment
-    # https://stackoverflow.com/questions/46785786/how-to-convert-xml-into-html-using-xslt-2-transformation-from-python3#comment80518546_46785786
-    # lxml python library is based on libxslt which only supports XSLT 1.0
-    # Stylesheets like tabella/1882/stylesheet.xsl need some Xpath functions to handle date formats properly
-    # those functions are only available on XSLT 2.0 
-    #
-    # We need to use an external processor who supports XSLT 2.0
-    # Probably Saxon processor. Needs JVM though
-    #
-    # Edit: SaxonC is Saxon compiled for C/C++ platforms 
-    # SaxonC API for python is available here https://pypi.org/user/saxonica/
-    # Only Home edition (saxonc-he) is non commercial but license should be studied because 
-    # some components are under mozilla license
-    #
-    # Here for more https://www.saxonica.com/saxon-c/index.xml
-
-    # NOTE
-    # One could easily request a trascription generated with an improper stylesheet.
-    # There is nothing indicating the doc type of ID/data.xml 
 
     STRUCT_TRASCR_PREFIX = "./data/transcr/struct/"
+    #
+    # len("data.xml") = 8
+    #
+    # s o m e / d a t a . x  m  l
+    # 0 1 2 3 4 5 6 7 8 9 10 11 12
+    #
+    # s[-8] = 5
+    # s[:-8] = s [0:5] (5 excluded)
+    #
+    # s o m e /
+    # 0 1 2 3 4
+    #
+    if Path(docKey).parts[-1] == "data.xml":
+        docKey = docKey[:- len("data.xml")]
+    # Path traversal attack should be covered
+    PROBABLE_CONTENT = send_from_directory(STRUCT_TRASCR_PREFIX, Path(docKey) / "data.xml", mimetype="text/xml")
+    print(f"From {STRUCT_TRASCR_PREFIX} serving {Path(docKey) / 'data.xml'}", flush=True)
+    if PROBABLE_CONTENT.status_code == 404:
+        flask.abort("Directory traversal", 404)
+    # Sending the file would pass sanity check. But since i need to read the file
+    # in memory to read schema url and validate against and send_from_directory
+    # sets direct_sendthrough=True i need an explicit file.open
+    # PROBABLE_CONTENT.get_data(as_text=True) doesn't work
 
-    data_xml_path = STRUCT_TRASCR_PREFIX + docKey + "/data.xml"
+    xml_path = Path(STRUCT_TRASCR_PREFIX) / docKey / "data.xml"
+    print(f"Opening {xml_path}",flush=True)
+    with open(xml_path, "r") as xml_fd:
+        document_content = xml_fd.read()
+        file_dom = xml.dom.minidom.parseString(document_content)
+    root_el : xml.dom.minidom.Element = file_dom.documentElement
+    print(f"{root_el}",flush=True)
 
-    schema_path = MAP_RUOTE_TO_FILESYSTEM["prefisso-filesystem"] + MAP_RUOTE_TO_FILESYSTEM["tipologie-documentali"][docType]["versioni-anni"][yearVersion]["tipologie-risorse"][apiXmlResource.SCHEMA.value]
+    schema_url = root_el.getAttribute("xsi:noNamespaceSchemaLocation")
 
-    transform_path = MAP_RUOTE_TO_FILESYSTEM["prefisso-filesystem"] + MAP_RUOTE_TO_FILESYSTEM["tipologie-documentali"][docType]["versioni-anni"][yearVersion]["tipologie-risorse"][apiXmlResource.STYLE.value]
+    print(f"{schema_url}", flush=True)
 
+    url_path_parts = Path(urllib.parse.urlparse(schema_url).path).parts
 
-    with open(data_xml_path) as f:
-        struct_data = etree.parse(f)
+    print(f"{url_path_parts}", flush=True)
+    if (not(docType in url_path_parts)) or (not(yearVersion in url_path_parts)):
+        return "Schema declared in url and the one declared in xml content do not match"
 
-    with open(schema_path) as f:
-        schema = etree.XMLSchema(etree.parse(f))
+    custom_parser = etree.XMLParser(load_dtd=True, no_network=False, huge_tree=True, resolve_entities=True)
+    custom_parser.resolvers.add(SimpleResolver())
 
-    with open(transform_path) as f:
-        transform = etree.XSLT(etree.parse(f))
+    schema= etree.XMLSchema(etree.fromstring(requests.get(schema_url).content, custom_parser))
+    struct_data = etree.parse(xml_path)
 
+    print(f"{schema}", flush=True)
     if not schema.validate(struct_data):
-        return "Error : data did not validate against candidate schema"
+        return '''Semantic error in URL : The document corresponding to the requested ID
+                    cannot be processed with the tools relative to the requested DOC type'''
 
-    result = transform(struct_data)
-    binc = etree.tostring(result, pretty_print=True, encoding='utf-8')
-    return binc.decode('utf-8')
-
-
-
-@app.route("/v1/schema/<docType>/<yearVersion>/<api_xmlResource:resourceType>")
-def send_schema(docType,yearVersion,resourceType):
-    path_str = None
-    try:
-        path_str = MAP_RUOTE_TO_FILESYSTEM["prefisso-filesystem"] + MAP_RUOTE_TO_FILESYSTEM["tipologie-documentali"][docType]["versioni-anni"][yearVersion]["tipologie-risorse"][resourceType]
-    except KeyError :
-        return "Some Error"
-    
-    # TODO Sanitizing path still looks like a good idea
-
-    content = None
-    with open(Path(path_str),"r") as fd:
-        content = fd.read()
-    if not (content is None) :
-        return Response(content,mimetype="text/xml")
-    else:
-        return "Some other Error"
+    print("Returning probable content",flush=True)
+    style_url : str = struct_data.xpath("//processing-instruction('xml-stylesheet')")[0].get("href")
+    same_origin_mirror = style_url.replace("https://gitlab.tools.iit.cnr.it",f"{os.environ['ROOT_URL']}/mirror_gitlab_tools_iit_cnr_it")
+    return Response(document_content.replace(style_url,same_origin_mirror),200,mimetype="text/xml")
